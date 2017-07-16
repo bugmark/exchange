18bd0bae6c27a218d9f29c702c96889150388316
andyl <andy@r210.com>
2017-07-15 23:19:24 -0700

    Change a to src

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,6 +12,6 @@ set :branch,    'master'
 
 set :rails_env, 'production'
 
-role :app, ['deploy@mvscim.net']
-role :db,  ['deploy@mvscim.net']
-role :web, ['deploy@mvscim.net']
+role :app, ['deploy@bugmark.net']
+role :db,  ['deploy@bugmark.net']
+role :web, ['deploy@bugmark.net']
