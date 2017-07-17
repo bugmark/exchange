8c24b4a1f6ac2ffc9abbf34db88a06dfa9ff6e5f
andyl <andy@r210.com>
2017-07-17 14:55:25 -0700

    Update DB tables

--- db/migrate/20150000000000_create_tables.rb
+++ db/migrate/20150000000000_create_tables.rb
@@ -1,23 +1,33 @@
 class CreateTables < ActiveRecord::Migration[5.1]
   def change
     create_table :repos do |t|
-      t.string   :format          # GitHub, CVRF
+      t.string   :type            # Repo::BugZilla, Repo::GitHub, Repo::Cvrf,
       t.string   :name            # mvscorg/xdmarket
       t.string   :url
+      t.jsonb    :xfields,  null: false, default: '{}'
       t.datetime :synced_at
       t.timestamps
     end
+    add_index :repos, :type
+    add_index :repos, :xfields, using: :gin
 
     create_table :bugs do |t|
       t.integer  :repo_id
+      t.string   :type             # Bug::BugZilla, Bug::GitHub, Bug::Cvrf
       t.string   :api_url
       t.string   :http_url
       t.string   :title
+      t.string   :description
       t.string   :status
       t.text     :labels, array: true, default: []
+      t.jsonb    :xfields,  null: false, default: '{}'
       t.datetime :synced_at
       t.timestamps
     end
+    add_index :bugs, :repo_id
+    add_index :bugs, :type
+    add_index :bugs, :labels , using: :gin
+    add_index :bugs, :xfields, using: :gin
 
     create_table :contracts do |t|
       t.string   :type
