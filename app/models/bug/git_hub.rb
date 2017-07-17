37ec07b0233bc96f2c5f8beeac211fd6102a3eff
andyl <andy@r210.com>
2017-07-17 15:21:04 -0700

    Add Repo and Bug sub-classes

---
+++ app/models/bug/git_hub.rb
@@ -0,0 +1,24 @@
+class Bug::GitHub < Bug
+
+  # belongs_to :repo
+
+end
+
+# == Schema Information
+#
+# Table name: bugs
+#
+#  id          :integer          not null, primary key
+#  repo_id     :integer
+#  type        :string
+#  api_url     :string
+#  http_url    :string
+#  title       :string
+#  description :string
+#  status      :string
+#  labels      :text             default([]), is an Array
+#  xfields     :jsonb            not null
+#  synced_at   :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
