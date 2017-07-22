b9e93b01661445a388544ed3193b3cef21df0b1d
andyl <andy@r210.com>
2017-07-22 16:39:49 -0700

    Add access control for contraction publish/take

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,11 +1,12 @@
 class ContractsController < ApplicationController
 
+  before_action :authenticate_user!, :except => [:index]
+
   def index
     @contracts = Contract.all
   end
 
   def new
-
   end
 
   def edit
