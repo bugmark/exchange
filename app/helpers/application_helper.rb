b068f11bff41d6513b533781f64706cef8f8d1c8
andyl <andy@r210.com>
2017-07-21 18:05:29 -0700

    Update devise configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
 
-  def nav_link(label, path)
+  def nav_link(label, path, opts = {})
+
+    delopt = opts[:method] == "delete"
+
+    delstr = delopt ? "rel='nofollow' data-method='delete'" : ""
 
     ext1, ext2 = if current_page?(path) then
       ["active", '<span class="sr-only">(current)</span>']
@@ -10,7 +14,7 @@ module ApplicationHelper
 
     raw %{
       <li class="nav-item #{ext1}">
-        <a class="nav-link" href="#{path}">#{label} #{ext2}</a>
+        <a class="nav-link" #{delstr} href="#{path}">#{label} #{ext2}</a>
       </li>
     }
   end
