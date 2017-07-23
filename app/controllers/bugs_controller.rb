5d0c7f6cc3a6a4c2b4beecca6927583fad91effa
andyl <andy@r210.com>
2017-07-23 08:22:19 -0700

    Add bug and contract filtering

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,5 +1,12 @@
 class BugsController < ApplicationController
+  # repo_id (optional)
   def index
-    @bugs = Bug.all
+    if repo_id = params["repo_id"]&.to_i
+      @repo = Repo.find(repo_id)
+      @bugs = Bug.where(repo_id: repo_id)
+    else
+      @repo = nil
+      @bugs = Bug.all
+    end
   end
 end
--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -2,17 +2,24 @@ class ContractsController < ApplicationController
 
   before_action :authenticate_user!, :except => [:index]
 
+  # bug_id (optional)
   def index
-    @contracts = Contract.all
+    if bug_id = params["bug_id"]&.to_i
+      @bug       = Bug.find(bug_id)
+      @contracts = Contract.where(bug_id: bug_id)
+    else
+      @bug       = nil
+      @contracts = Contract.all
+    end
   end
 
+  # bug_id or repo_id
   def new
-    binding.pry
-    @contract = Contract.new
+    @contract = Contract.new(type: 'Contract::Reward')
   end
 
+  # id (contract ID)
   def edit
-    binding.pry
-    x = 1
+    @contract = Contract.find(params[:id])
   end
 end
