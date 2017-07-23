8bef8900b5059bb2252351d7f5c4d9bcab864c9b
andyl <andy@r210.com>
2017-07-23 10:29:25 -0700

    Update contract associations

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -24,8 +24,9 @@ class ContractsController < ApplicationController
   end
 
   def create
-    binding.pry
-    xx = 1
+    opts = params["contract_forecast"] || params["contract_reward"]
+    contract = Contract.create(valid_params(opts))
+    redirect_to("/contracts/#{contract.id}")
   end
 
   def show
@@ -34,11 +35,23 @@ class ContractsController < ApplicationController
 
   private
 
+  def valid_params(params)
+    fields = %i(type terms bug_id repo_id currency_type currency_amount expire_at publisher_id)
+    params.permit(fields)
+  end
+
   def new_opts(params)
-    opts = {type: "Contract::#{params["type"]&.capitalize}"}
+    opts = {
+      type:            "Contract::#{params["type"]&.capitalize}"    ,
+      terms:           "Net0"                                       ,
+      currency_type:   "PokerBux"                                   ,
+      currency_amount: 10                                           ,
+      expire_at:       Time.now + 14.days                           ,
+      publisher_id:    current_user.id
+    }
     key  = "bug_id"  if params["bug_id"]
     key  = "repo_id" if params["repo_id"]
     id   = params["bug_id"] || params["repo_id"]
-    opts.merge({key => id, 'publisher_id' => current_user.id})
+    opts.merge({key => id})
   end
 end
--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -4,6 +4,12 @@ module ContractsHelper
     raw "<a href='/contracts/#{contract.id}'>#{type}</a>"
   end
 
+  def contract_attach_link(contract)
+    id   = contract.bug_id || contract.repo_id
+    type = contract.bug_id ? "bugs" : "repos"
+    raw "<a href='/#{type}/#{id}'>#{type[0..-2].capitalize} #{id}</a>"
+  end
+
   def contract_take_link(contract)
     status = contract.status
     if status == "open"
