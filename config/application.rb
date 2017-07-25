93ef64694f08e898a6fccaa024b09dcbaa87f714
andyl <andy@r210.com>
2017-07-25 07:33:22 -0700

    Change services to forms

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Bugmark
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.autoload_paths += %W(#{config.root}/app/services)
+    config.autoload_paths += %W(#{config.root}/app/forms)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
