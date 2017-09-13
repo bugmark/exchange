5e89ad400e3943298919d1e2a21ac17b7be0a598
andyl <andy@r210.com>
2017-09-13 15:52:41 -0700

    Fix offer rendering bug

---
+++ spec/features/predictions_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Predictions" do
+
+  let(:user)     { FG.create(:user).user              }
+  let(:contract) { Contract.create                    }
+  let(:taken)    { FG.create(:taken_contract)         }
+
+  it "renders index" do
+    visit "/predictions"
+    expect(page).to_not be_nil
+  end
+end
