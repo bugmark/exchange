e9b205f380cf341043185039b2c47320ea8d9739
andyl <andy@r210.com>
2017-10-06 13:25:07 -0700

    Add contracts feature spec

--- spec/features/core/contracts_spec.rb
+++ spec/features/core/contracts_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-describe "Rewards" do
+describe "Contracts" do
 
   let(:user)     { FG.create(:user).user              }
   let(:contract) { Contract.create                    }
   let(:taken)    { FG.create(:taken_contract)         }
 
-  # it "renders index" do
-  #   visit "/core/rewards"
-  #   expect(page).to_not be_nil
-  # end
-  #
+  it "renders index" do
+    visit "/core/contracts"
+    expect(page).to_not be_nil
+  end
+
   # it "renders show" do
-  #   visit "/core/rewards/#{contract.id}"
+  #   visit "/core/contracts/#{contract.id}"
   #   expect(page).to_not be_nil
   # end
 end
