3f05f5e037afc7cfffadaaa81ab296973a3510ba
andyl <andy@r210.com>
2017-10-15 11:34:01 -0700

    Update annotations

--- app/models/offer/buy/ask.rb
+++ app/models/offer/buy/ask.rb
@@ -20,3 +20,32 @@ class Offer::Buy::Ask < Offer::Buy
     self.maturation_period  ||= Time.now+1.minute..Time.now+1.week
   end
 end
+
+# == Schema Information
+#
+# Table name: offers
+#
+#  id                  :integer          not null, primary key
+#  type                :string
+#  repo_type           :string
+#  user_id             :integer
+#  parent_id           :integer
+#  position_id         :integer
+#  counter_id          :integer
+#  volume              :integer          default(1)
+#  price               :float            default(0.5)
+#  poolable            :boolean          default(TRUE)
+#  aon                 :boolean          default(FALSE)
+#  status              :string
+#  offer_expiration    :datetime
+#  contract_maturation :datetime
+#  maturation_period   :tsrange
+#  repo_id             :integer
+#  bug_id              :integer
+#  bug_title           :string
+#  bug_status          :string
+#  bug_labels          :string
+#  jfields             :jsonb            not null
+#  exref               :string
+#  uuref               :string
+#
