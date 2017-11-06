585ea9ed190f7a0b741d5c85806b16b917f8137a
andyl <andy@r210.com>
2017-11-06 12:36:32 -0800

    Update docfix pages

--- app/controllers/docfix/offers_controller.rb
+++ app/controllers/docfix/offers_controller.rb
@@ -4,9 +4,11 @@ module Docfix
     layout 'docfix'
 
     def index
+      @offers = Offer.paginate(page: params[:page], per_page: 5)
     end
 
     def show
+      @offer = Offer.find(params["id"])
     end
   end
 end
