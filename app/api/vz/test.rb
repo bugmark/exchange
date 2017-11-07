3e71588d60d8824723c3e82aac14363e549aa60d
andyl <andy@r210.com>
2017-11-07 10:09:13 -0800

    Remove all API code

--- app/api/vz/test.rb
+++ app/api/vz/test.rb
@@ -1,12 +1,12 @@
-module Vz
-  class Test < Grape::API
-
-    resource :issues do
-      desc "Return all issues"
-      get "", :root => :issues do
-        Bug.all
-      end
-    end
-
-  end
-end
+# module Vz
+#   class Test < Grape::API
+#
+#     resource :issues do
+#       desc "Return all issues"
+#       get "", :root => :issues do
+#         Bug.all
+#       end
+#     end
+#
+#   end
+# end
