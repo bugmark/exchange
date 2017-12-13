8c9b2fa03cd15568ba2a99f3c9a1c152a41f9442
Rich Bodo <richbodo@gmail.com>
2017-12-13 16:21:05 +0000

    correct output file

--- eth-es/reports/download_events.sh
+++ eth-es/reports/download_events.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X GET --header 'Accept: application/json' 'https://bugmark.net/api/v1/events'
+curl -X GET --header 'Accept: application/json' 'https://bugmark.net/api/v1/events' > eventstore.txns.json
