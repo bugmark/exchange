673389e366d821bf9c3e80f208fac1a763b2b2c1
andyl <andy@r210.com>
2019-01-25 17:01:13 -0800

    Update cmd bash

--- script/docker/cmd.bash
+++ script/docker/cmd.bash
@@ -5,11 +5,9 @@ export POSTGRES_HOST=postgres
 echo "----- BUNDLE INSTALL -----"
 bundle install
 
-echo "----- DB CREATE -----"
-rails db:create
-
-echo "----- DB MIGRATE -----"
-rails db:migrate
-
-echo "----- RAILS SERVER -----"
-rails server -b 0.0.0.0
+echo "----- DB CREATE -----" \
+  && rails db:create \
+  && echo "----- DB MIGRATE -----" \
+  && rails db:migrate \
+  && echo "----- RAILS SERVER -----" \
+  && rails server -b 0.0.0.0 \
